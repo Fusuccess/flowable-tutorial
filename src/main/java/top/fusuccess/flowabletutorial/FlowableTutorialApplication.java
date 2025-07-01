ab9c16feee7baf7657c785d3ec93a5702e1f6ce5
Fusuccess <fu2000520@tom.com>
2025-07-01 11:18:05 +0800

    init

---
+++ src/main/java/top/fusuccess/flowabletutorial/FlowableTutorialApplication.java
@@ -0,0 +1,13 @@
+package top.fusuccess.flowabletutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/fusuccess/flowabletutorial/FlowableTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fusuccess.flowabletutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
