3dbe8c1fc753e47f0d69bb96356b32cd48c122ea
Fusuccess <fu2000520@tom.com>
2025-07-03 19:43:32 +0800

    ✨ Add 服务任务（Service Task）

---
+++ src/main/java/top/fusuccess/flowabletutorial/tutorial_serviceTask/MailSendImpl.java
@@ -0,0 +1,10 @@
+package top.fusuccess.flowabletutorial.tutorial_serviceTask;
+
+public class MailSendImpl {
+   public void sendMail(String from,String to,String subject,String content ) {
+
+       System.err.println("======================发送邮件开始======================");
+       System.err.println("发送邮件成功！");
+       System.err.println("======================发送邮件结束======================");
+    }
+}
---
+++ src/main/java/top/fusuccess/flowabletutorial/tutorial_serviceTask/ServicesTask.java
@@ -0,0 +1,20 @@
+package top.fusuccess.flowabletutorial.tutorial_serviceTask;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+public class ServicesTask implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+        Boolean skipTask = (Boolean) execution.getVariable("skipTask");
+        if (skipTask != null && skipTask) {
+            System.out.println("Skipping the service task!");
+        } else {
+            System.out.println("Executing the service task!");
+            // 执行你的业务逻辑
+            MailSendImpl mailSend = new MailSendImpl();
+            mailSend.sendMail("fusuccess@163.com","fusuccess@163.com","测试邮件","测试邮件");
+        }
+    }
+
+}
